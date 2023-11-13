20c21fe5cc62ca67b1b4d6fe543abd4f9b409750
Codesmaster-Lab <codesmaster404@gmail.com>
2023-11-13 17:12:35 +0530

    wf27

--- projects/sdec-common-lib/src/lib/header/header.component.ts
+++ projects/sdec-common-lib/src/lib/header/header.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  // toggleNavbar() {
+  //   const navbarLinks = document.querySelector('.navbar-links');
+  //   navbarLinks.style.display = (navbarLinks.style.display === 'flex' || navbarLinks.style.display === '') ? 'none' : 'flex';
+  // }
 }
